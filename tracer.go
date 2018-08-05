4141a596b5545e2c92bbff496d86155c8e81b52e
tomocy <tomocy.dev@gmail.com>
2018-08-05 19:18:39 +0900

    add struct to implment Tracer

--- tracer.go
+++ tracer.go
@@ -1,5 +1,22 @@
 package tracer
 
+import (
+	"fmt"
+	"io"
+)
+
 type Tracer interface {
 	Trace(...interface{})
+}
+
+func New(w io.Writer) Tracer {
+	return &tracer{out: w}
+}
+
+type tracer struct {
+	out io.Writer
+}
+
+func (t *tracer) Trace(a ...interface{}) {
+	t.out.Write([]byte(fmt.Sprint(a...)))
 }
--- tracer_test.go
+++ tracer_test.go
@@ -1,7 +1,22 @@
 package tracer
 
-import "testing"
+import (
+	"bytes"
+	"testing"
+)
 
 func TestNew(t *testing.T) {
-	t.Error("this test is not implemented")
+	var buf bytes.Buffer
+	tracer := New(&buf)
+	if tracer == nil {
+		t.Error("tracer is nil")
+		return
+	}
+
+	want := "hello, tracer\n"
+	tracer.Trace(want)
+	have := buf.String()
+	if have != want {
+		t.Errorf("have %v, but want %v", have, want)
+	}
 }
